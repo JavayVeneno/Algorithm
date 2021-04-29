bff3680cccb2251698b1bdbf10131526db4f3223
Veneno <747433822@qq.com>
2021-04-29 22:15:05 +0800

    [第一周][0429]简单的线性查找实现自定义类的查找

--- LinearSearch/src/LinearSearch.java
+++ LinearSearch/src/LinearSearch.java
@@ -2,20 +2,37 @@ public class LinearSearch {
    private  LinearSearch(){
 
    }
-    public static int search(int[] data ,int target){
+    public static <E> int search(E[] data ,E target){
 
         for(int i = 0;i<data.length;i++){
-            if(data[i]==target){
+            if(data[i].equals(target)){
                 return i;
             }
         }
         return -1;
     }
     public static void main(String[] args){
-        int[] data = {24,18,12,9,16,66,32,4};
-        int res = LinearSearch.search(data,16);
-        int res2 = search(data,666);
-        System.out.println(res);
-        System.out.println(res2);
+//        Integer[] data = {24,18,12,9,16,66,32,4};
+//        int res = LinearSearch.search(data,16);
+//        int res2 = search(data,666);
+//        System.out.println(res);
+//        System.out.println(res2);
+
+        Student s1 = new Student(1L,"老刘",30);
+        Student s2 = new Student(2L,"胜英",29);
+        Student s3 = new Student(3L,"云神",28);
+        Student s4 = new Student(4L,"海军",27);
+        Student s5 = new Student(4L,"张海军",26);
+        Student s6 = new Student(5L,"王波",26);
+        Student s7 = new Student(6L,"志伟",24);
+        Student[] ennovations = {s1,s2,s3,s4,s5,s6,s7};
+        Student longjie  = new Student(7L,"龙杰",27);
+        Student who  = new Student();
+        who.setId(4L);
+        int indexofhaijun = search(ennovations,who);
+        int indexoflongjie = search(ennovations,longjie);
+        System.out.println(indexofhaijun);
+        System.out.println(indexoflongjie);
+        System.out.println(s4.equals(s5));
     }
 }
---
+++ LinearSearch/src/Student.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Student {
+    public Student(){
+
+    }
+
+    public Student(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
