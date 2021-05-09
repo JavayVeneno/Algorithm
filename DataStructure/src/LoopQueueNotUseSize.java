cd47e1adb37ae0456352d51c7dc03f5f8d23822f
Veneno <747433822@qq.com>
2021-05-09 23:12:16 +0800

    [第三周][0509]改成一种不使用size的方式,但是扩容出现了一点问题,另一种不多占用空间,暂未实现。

---
+++ DataStructure/src/LoopQueueNotUseSize.java
@@ -0,0 +1,116 @@
+public class LoopQueueNotUseSize<E> implements Queue<E> {
+
+    private E[] data;
+    private int front,tail;
+
+    @SuppressWarnings("unchecked")
+    public LoopQueueNotUseSize(int capacity){
+        data = (E[]) new Object[capacity+1];
+        front = 0;
+        tail = 0;
+    }
+
+    public LoopQueueNotUseSize(){
+        this(10);
+    }
+
+    public int getCapacity(){
+        return data.length-1;
+    }
+
+    @Override
+    public int getSize() {
+        return tail>=front?tail-front:tail+data.length-front;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return tail==front;
+    }
+
+    @Override
+    public E getFront() {
+        if(isEmpty()){
+            throw new IllegalArgumentException("cannot dequeue from an empty queue");
+        }
+        return data[front];
+    }
+
+    @Override
+    public void enqueque(E e) {
+        if(tail%data.length+1==front){
+            resize(getCapacity()<<1);
+        }
+        data[tail]=e;
+        tail=tail%data.length+1;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int capacity) {
+        E[] newData = (E[]) new Object[capacity+1];
+        int sz = getSize();
+        for (int i = front; i%data.length != tail%data.length ; i++) {
+            newData[(i-front)] = data[i%data.length];
+        }
+        front = 0;
+        tail =sz;
+        data = newData;
+
+    }
+
+    @Override
+    public E dequeque() {
+        if(isEmpty()){
+            throw new IllegalArgumentException("cannot dequeue from an empty queue");
+        }
+        E res =  data[front];
+        data[front] = null;
+        front  = (front+1)%data.length;
+
+        if(getSize()==getCapacity()>>2 && getCapacity()>>1 !=0){
+            resize(getCapacity()>>1);
+        }
+
+        return res;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Queue size = %d , capacity = %d %n",getSize(),getCapacity()));
+        sb.append("front [");
+//        for (int i = front; i !=tail ; i=(i+1)%data.length) {
+//            sb.append(data[i]);
+//
+//            if((i+1)%data.length !=tail){
+//                sb.append(',');
+//            }
+//
+//        }
+
+        for (int i = 0; i <getSize() ; i++) {
+            sb.append((i+front)%data.length);
+            if ((i+front)%data.length+1 != tail){
+                sb.append(',');
+            }
+        }
+        sb.append("] tail");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LoopQueueNotUseSize<Integer> queue = new LoopQueueNotUseSize<>(10);
+        for (int i = 0; i < 10 ; i++) {
+            queue.enqueque(i);
+            System.out.println(queue);
+            if(i%3 == 2){
+                queue.dequeque();
+                System.out.println(queue);
+            }
+
+
+        }
+        System.out.println(queue);
+    }
+}
