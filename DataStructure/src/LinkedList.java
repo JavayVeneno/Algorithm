e7f453a0bcd51ebbfd066dcfc04ed59100114195
Veneno <747433822@qq.com>
2021-05-22 21:42:28 +0800

    [第四周][0522]linkedList中增删改查基本都是O(n)的操作,看起来是全方位的弱于数组,但是我们只操作头节点,优势就会体现。

--- DataStructure/src/LinkedList.java
+++ DataStructure/src/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class LinkedList<E> {
 
 
@@ -38,6 +40,7 @@ public class LinkedList<E> {
         size = 0;
     }
 
+    // 时间复杂度分析,O(1)
     public void addFirst(E e){
        // 在头部添加一个元素时,这个元素的next就应该指向原头部,自己再成为新头部
 //        Node node = new Node(e);
@@ -47,8 +50,9 @@ public class LinkedList<E> {
 //        head = new Node(e,head);
         // 使用 dummyHead可以复用Add
         add(0,e);
-        size++;
+//        size++;
     }
+    // 时间复杂度分析,O(n)
     public void add(int index,E e){
         if(index < 0 || index > size){
             throw new RuntimeException("can not add with wrong index");
@@ -74,8 +78,115 @@ public class LinkedList<E> {
 //        }
 
     }
+    // 时间复杂度分析,O(n)
     public void addLast(E e){
         add(size,e);
     }
 
+
+    // 时间复杂度分析,O(n)
+    public E get(int index){
+        if(index < 0 || index > size){
+            throw new RuntimeException("can not get with wrong index");
+        }
+        Node current = dummyHead.next;
+        for (int i = 0; i <index ; i++) {
+            current = current.next;
+        }
+        return current.e;
+    }
+
+    // 时间复杂度分析,O(1)
+    public E getFirst(){
+        return get(0);
+    }
+
+    // 时间复杂度分析,O(n)
+    public E getLast(){
+        return get(size-1);
+    }
+
+    // 时间复杂度分析,O(n)
+    public void set(int index,E e){
+        if(index < 0 || index > size){
+            throw new RuntimeException("can not set with wrong index");
+        }
+        Node current = dummyHead.next;
+        for (int i = 0; i <index ; i++) {
+            current = current.next;
+        }
+        current.e = e;
+    }
+
+    // 时间复杂度分析,O(n)
+    public boolean contains(E e){
+        Node current = dummyHead.next;
+
+        while (current != null ){
+            if(Objects.equals(current.e,e)){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    // 时间复杂度分析,O(n)
+    public E remove(int index){
+        if(index < 0 || index > size){
+            throw new RuntimeException("can not remove with wrong index");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i <index ; i++) {
+            prev = prev.next;
+        }
+        Node current = prev.next;
+        prev.next = current.next;
+        current.next = null;
+        size--;
+        return current.e;
+    }
+
+    // 时间复杂度分析,O(1)
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    // 时间复杂度分析,O(n)
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Node current = dummyHead.next;current!=null;current=current.next){
+            sb.append(current.e).append("->");
+        }
+        sb.append("NULL");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2,666);
+        System.out.println(linkedList);
+        System.out.println(linkedList.contains(666));
+        System.out.println(linkedList.contains(6));
+        System.out.println(linkedList.get(2));
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
 }
