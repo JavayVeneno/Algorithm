09b76b1c44c09fec6a650617152c2d2720fa8a66
Veneno <747433822@qq.com>
2021-06-01 22:47:14 +0800

    [第四周][0601]使用循环以及递归两种方式实现翻转链表。循环时需要三个指针来指定prev,current,next来依次交换。递归的思路是链表不变,左边未翻转,右边已经翻转,左边尾节点的next指向的已经翻转的尾节点,此时需要让左边尾节点的next(即右边尾节点)的next指向左边节点(head.next.next=head)。

---
+++ leetcode/src/linkedlist/Solution4.java
@@ -0,0 +1,60 @@
+package linkedlist;
+
+
+
+/**
+ *  翻转链表非递归实现
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution4 {
+
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null){
+            return null;
+        }
+//        ListNode prev = null;
+//        ListNode current = head;
+//        ListNode next = head.next;
+//
+//        while(next != null){
+//            current.next = prev;
+//            prev = current;
+//            current =next;
+//            next = next.next;
+//        }
+//        current.next=prev;
+//        return current;
+
+
+        return recursiveReverse(head);
+    }
+
+
+    public ListNode recursiveReverse (ListNode head) {
+
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode ok = reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return ok;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode test  = new ListNode(new int[]{1,2,3,4});
+        System.out.println(test);
+        ListNode listNode = new Solution4().reverseList(test);
+        System.out.println(listNode);
+    }
+}
