4b010e0cbe406b4b8f14f343c01fd4e6d580b5ce
Veneno <747433822@qq.com>
2021-07-09 21:19:33 +0800

    [第六周][0709]quick sort的一个优化使用随机索引处的元素作为基准点与第一个点做交换,并且设计一个方法生成的数组是可以使quick sort在取中间点作为标定点的方式退化成O(n²)的算法

--- common/src/ArrayGenerator.java
+++ common/src/ArrayGenerator.java
@@ -24,4 +24,38 @@ public class ArrayGenerator {
         return arr;
     }
 
+    // 生成一组数据在quick sort每次选用最中间的标定点都为此时的最小值
+
+    public static Integer[] generatorArrayPartition(int n){
+
+        Integer[] arr = new Integer[n];
+
+        partitionArray(arr,0,0,arr.length-1);
+
+        return arr;
+    }
+
+    private static void partitionArray(Integer[] arr, int value, int l, int r) {
+        if(l>r){
+            return;
+        }
+        int mid = l+(r-l)/2;
+        arr[mid] = value;
+        swap(arr,mid,l);
+        partitionArray(arr,value+1,l+1,r);
+        swap(arr,mid,l);
+    }
+
+    private static <E extends Comparable<E>> void swap(E[] arr, int x, int y) {
+        E temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] integers = generatorArrayPartition(10);
+        for (Integer integer : integers) {
+            System.out.println(integer);
+        }
+    }
 }
--- sort/src/QuickSort.java
+++ sort/src/QuickSort.java
@@ -1,9 +1,14 @@
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Random;
 
 public class QuickSort {
 
+    static Random random;
     private QuickSort(){}
+    static{
+        random = new Random();
+    }
 
     public static  <E extends Comparable<E>> void  sort(E[] arr){
         quickSort(arr,0,arr.length-1);
@@ -71,7 +76,22 @@ public class QuickSort {
 
 
     public  static <E extends Comparable<E>> int partition(E[] arr, int l, int r) {
+        // 选择第一个元素为基准点在测试用例为有序数组时会导致时间复杂度变成O(n²),
+        // 因为每次标定后,标定点都是最小元素,即左边区间为空,右边为n-1,n-2...n-n
+        // 递归深度也将变成 O(n),即n次递归调用,n超过一定规模直接StackOverflow
+        // 所以我们需要随机选择一个元素作为标定点,在[l,r]这个区间随机选择一个元素,与l做交换
+        // 因为没有现成的api可以生成[l,r]的方法,但是有[0,n)这样的api,所以我们需要l+[0,r-l]即为[l,r]
+        int p = l + random.nextInt(r-l+1);
 
+        // 假设每次选中间,理论上时让左右区间更加平衡,也减少递归深度,但是我们可以整出一个测试用例让每次的中间值都是最小值
+        //  当我们设计了(ArrayGenerator.generatorArrayPartition)
+        //这样的测试用例就会使quick sort退化至O(n²)的时间复杂度 如下:
+        //QuickSort.sort sort 100000 data  use 4.943561 s
+        //MergeSort.sort sort 100000 data  use 0.034707 s
+
+        //int p = l+(r-l)/2;
+
+        swap(arr,l,p);
         // 以第一个元素为基准点
         // 那么接下来可以将数组分成三部分 即 part这个基准点 + 小于part的部分 + 大于等part的部分
         // 即 [l] + [l+1...j]<part + [j+1...r]>=part
@@ -110,18 +130,25 @@ public class QuickSort {
 
 
         Integer[] arrs = ArrayGenerator.generatorRandomArray(1000000 ,1000000 );
-        Integer[] arrs2 = Arrays.copyOf(arrs, arrs.length);
-        Integer[] arrs3 = Arrays.copyOf(arrs, arrs.length);
-        Integer[] arrs4 = Arrays.copyOf(arrs, arrs.length);
-        Integer[] arrs5 = Arrays.copyOf(arrs, arrs.length);
-        Integer[] arrs6 = Arrays.copyOf(arrs, arrs.length);
-        Integer[] arrs7 = Arrays.copyOf(arrs, arrs.length);
-        SortingHelper.sortTest(QuickSort.class,"sort",arrs);
-        SortingHelper.sortTest(QuickSort.class,"sort2",arrs2);
-        SortingHelper.sortTest(QuickSort.class,"sort3",arrs3);
-        SortingHelper.sortTest(QuickSort.class,"sort4",arrs4);
-        SortingHelper.sortTest(MergeSort.class,"sort4",arrs5);
-        SortingHelper.sortTest(MergeSort.class,"sort5",arrs6);
-        SortingHelper.sortTest(MergeSort.class,"sort2",arrs7);
+        Integer[] test = ArrayGenerator.generatorArrayPartition(100000);
+        Integer[] test2 = Arrays.copyOf(test, test.length);
+
+//        Integer[] arrs2 = Arrays.copyOf(arrs, arrs.length);
+//        Integer[] arrs3 = Arrays.copyOf(arrs, arrs.length);
+//        Integer[] arrs4 = Arrays.copyOf(arrs, arrs.length);
+//        Integer[] arrs5 = Arrays.copyOf(arrs, arrs.length);
+//        Integer[] arrs6 = Arrays.copyOf(arrs, arrs.length);
+//        Integer[] arrs7 = Arrays.copyOf(arrs, arrs.length);
+//        SortingHelper.sortTest(QuickSort.class,"sort",arrs);
+//        SortingHelper.sortTest(QuickSort.class,"sort2",arrs2);
+//        SortingHelper.sortTest(QuickSort.class,"sort3",arrs3);
+//        SortingHelper.sortTest(QuickSort.class,"sort4",arrs4);
+//        SortingHelper.sortTest(MergeSort.class,"sort4",arrs5);
+//        SortingHelper.sortTest(MergeSort.class,"sort5",arrs6);
+//        SortingHelper.sortTest(MergeSort.class,"sort2",arrs7);
+
+        Integer[] integers = ArrayGenerator.generatorOrderArray(100000);
+        SortingHelper.sortTest(QuickSort.class,"sort",test);
+        SortingHelper.sortTest(MergeSort.class,"sort",test2);
     }
 }
