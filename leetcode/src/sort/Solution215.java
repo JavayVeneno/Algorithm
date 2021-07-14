19d4b30e7f2aca0682188790c24ee8907c121d6b
Veneno <747433822@qq.com>
2021-07-14 23:16:29 +0800

    使用双路快排的partition解决leetcode215题

---
+++ leetcode/src/sort/Solution215.java
@@ -0,0 +1,68 @@
+package sort;
+
+import java.util.Random;
+
+class Solution215 {
+
+    private static final Random random = new Random();
+
+    public int findKthLargest(int[] nums, int k) {
+        int maxIndex  = nums.length-k;
+
+
+        return nums[selectK(nums,maxIndex,0,nums.length-1)];
+    }
+
+
+    private int selectK(int[] nums, int maxIndex, int l , int r){
+        int p  = partition(nums,l,r);
+        if(p == maxIndex){
+            return p;
+        }else if(p>maxIndex){
+            return selectK(nums,maxIndex,l,p-1);
+        }else{
+            return selectK(nums,maxIndex,p+1,r);
+        }
+    }
+
+    private int partition(int[] nums, int l, int r) {
+        int p = l + random.nextInt(r - l + 1);
+        int value = nums[p];
+     swap(nums,l,p);
+     int i = l+1,j = r;
+     // [l,i-1]<=value,[j+1,r]>=value
+     while(true){
+         while(i<=j && nums[i]<value ){
+             i++;
+         }
+
+         while(j>=i && nums[j]>value){
+             j--;
+         }
+         if(i>=j){
+             break;
+         }
+         swap(nums,i,j);
+         i++;
+         j--;
+     }
+     swap(nums,l,j);
+       return j;
+
+    }
+
+    private void swap(int[] nums, int l, int p) {
+        if(l==p){
+            return ;
+        }
+        nums[l] = nums[l] ^ nums[p];
+        nums[p] = nums[l] ^ nums[p];
+        nums[l] = nums[l] ^ nums[p];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,3,1,2,4,5,5,6};
+        int kthLargest = new Solution215().findKthLargest(nums, 4);
+        System.out.println(kthLargest);
+    }
+}
