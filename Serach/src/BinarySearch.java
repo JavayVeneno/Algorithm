2e691d6f8c5e193f9d4bfa39f4103ce858e46477
Veneno <747433822@qq.com>
2021-07-16 22:04:43 +0800

    [第七周][0716]使用非递归的方式实现二分查找法

--- Serach/src/BinarySearch.java
+++ Serach/src/BinarySearch.java
@@ -19,10 +19,31 @@ public class BinarySearch {
             return search(data,l,middle-1,target);
         }
     }
+    public static <E extends Comparable<E>> int search2(E[] data,E target){
+        int l = 0, r = data.length-1;
+
+        // 循环不变量是 [l,r]区间仍然有值
+        while(l<=r){
+            int p  = l+(r-l)/2;
+            if(data[p].compareTo(target)==0){
+                return p;
+            }else if(data[p].compareTo(target)<0){
+                l = p+1;
+            }else{
+                r = p-1;
+            }
+        }
+        return -1;
+    }
+
+
+
 
     public static void main(String[] args) {
-        Integer[] test = {0,1,2,3,5,6,7,8};
-        int search = search(test, 8);
+        Integer[] test = {-1,0,3,5,9,12};
+        int search = search2(test,0);
         System.out.println(search);
+
+
     }
 }
