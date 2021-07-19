751afa6f1376d4985924fc4fd53c30218ba75357
Veneno <747433822@qq.com>
2021-07-19 23:13:56 +0800

    [第七周][0719]二分查找变种upper的实现:查找大于目标值的的最小值。其逻辑就是查找[0,length],即[l,r],根据计算的中间值来不断缩小[l,r]使其相等这(中值大于目标值,中值即为新的r值,反之l=中值+1,因为中值已经等于target),最终得出结果。当l=r=length时,越界,说明不存在。

--- Serach/src/BinarySearch.java
+++ Serach/src/BinarySearch.java
@@ -1,3 +1,5 @@
+import sun.plugin.com.event.COMEventHandler;
+
 public class BinarySearch {
     private BinarySearch(){}
 
@@ -36,14 +38,43 @@ public class BinarySearch {
         return -1;
     }
 
+    // 二分搜索的变种:upper,查找大于某值的最小值
+    // [l,m]<=value [m,r]>value 按此逻辑进行二分,那么最终只有lr值,[l,r]确定一个值
 
+    public static <E extends Comparable<E>> int binarySearchUpper(E[] arr,E target){
+        int i = binarySearchUpper(arr, 0, arr.length, target);
+        return i==arr.length?-1:i;
+    }
+
+    private static <E extends Comparable<E>> int binarySearchUpper(E[] arr, int l, int r, E target) {
+
+
+        while(l<r){
+            int middle = l+(r-l)/2;
+            // 假如中间值5,比4大
+            if(arr[middle].compareTo(target)>0){
+                // 说明我这个r的索引至少是middle 5
+                r=middle;
+            }
+            if(arr[middle].compareTo(target)<=0){
+               //反之我左边界最大值也只能等于middle值
+                // 所以需要排掉middle这个等于的值
+                l = middle+1;
+            }
+
+        }
+        return r;
+    }
 
 
     public static void main(String[] args) {
-        Integer[] test = {-1,0,3,5,9,12};
+        Integer[] test = {-1,0,0,3,3,5,9,12};
         int search = search2(test,0);
-        System.out.println(search);
 
+
+        System.out.println(search);
+        int i = binarySearchUpper(test, 2);
+        System.out.println(test[i]);
 
     }
 }
