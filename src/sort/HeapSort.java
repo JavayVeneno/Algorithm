d2b18236361deda29c4b501f02bef33daa4eebdb
Veneno <747433822@qq.com>
2021-08-05 22:12:47 +0800

    [第九周][0805]使用heapify构建堆,然后通过最大值与末端值交换,不断缩小未排序区域,siftDown未排序区域来实现堆排序。和add入堆之后来依次取最大值想比,优势不太明显在百万级数据时,快0.01-0.03秒。(多次测试不具统计学意义)

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -14,7 +14,8 @@ public class HeapSort<E extends Comparable<E>> {
 
     }
 
-    public static <E extends Comparable<E>> void sort(E[] data){
+    // 较为直观的堆排序
+    public static <E extends Comparable<E>> void sort2(E[] data){
         MaxHeap<E> heap = new MaxHeap<>();
         for (E e : data) {
             heap.add(e);
@@ -24,6 +25,48 @@ public class HeapSort<E extends Comparable<E>> {
         }
     }
 
+    // 优化后的堆排序(使用heapify的方式,然后依次取最大元素依次末端放置并siftdown被破坏之后的堆
+    public static <E extends Comparable<E>> void sort(E[] data){
+
+        if(data.length<=1){
+            return;
+        }
+
+        // 首先构建一个堆,从最后一个非叶子节点开始(lastIndex-1)/2 = (data.length-1 -1)/2;
+        for (int i = (data.length-2)/2; i >=0 ; i--) {
+            siftDown(data,i,data.length);
+        }
+
+        for (int i = data.length-1; i >=0 ; i--) {
+            //将首未交换之后,最大值已至末尾,此时除去末尾部位需要重新siftDown ,所以size = i;
+            swap(data,0,i);
+            siftDown(data,0,i);
+        }
+    }
+
+    private static <E extends Comparable<E>> void swap(E[] data, int a, int b) {
+        E temp = data[a];
+        data[a] = data[b];
+        data[b] =temp;
+    }
+
+    private static <E extends Comparable<E>> void siftDown(E[] data, int k, int size) {
+
+        while( 2 * k +1 <size){
+            int j = 2 * k +1;
+            if(j+1<size && data[j+1].compareTo(data[j])>0){
+                j++;
+            }
+            if(data[j].compareTo(data[k])<=0){
+                break;
+            }
+            swap(data,j,k);
+            k = j;
+        }
+
+    }
+
+
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
         int n = 1000000;
         Integer[] arr = ArrayGenerator.generatorRandomArray(n,n);
@@ -31,12 +74,14 @@ public class HeapSort<E extends Comparable<E>> {
         Integer[] arr2 = Arrays.copyOf(arr,arr.length);
         Integer[] arr3 = Arrays.copyOf(arr,arr.length);
         Integer[] arr4 = Arrays.copyOf(arr,arr.length);
+        Integer[] arr5 = Arrays.copyOf(arr,arr.length);
 
 
         SortingHelper.sortTest(QuickSort.class,"sort",arr);
         SortingHelper.sortTest(QuickSort.class,"sort3Ways",arr2);
         SortingHelper.sortTest(MergeSort.class,"sort4",arr3);
         SortingHelper.sortTest(HeapSort.class,"sort",arr4);
+        SortingHelper.sortTest(HeapSort.class,"sort2",arr5);
     }
 
 }
