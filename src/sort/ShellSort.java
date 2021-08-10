62be6652559693640ce41bd35b4c2709a5a78a66
Veneno <747433822@qq.com>
2021-08-10 22:48:10 +0800

    [第十周][0810] 实现使用insertion sort的shell sort。sort2复写insertion sort来实现:第一层循环控制步长,第二层循环控制每个子数组遍历,每次遍历都调用insertion sort。(第三层循环,遍历子数组中的每个元素,第四场循环,遍历每个未插入的元素)

---
+++ src/sort/ShellSort.java
@@ -0,0 +1,77 @@
+package sort;
+
+import common.ArrayGenerator;
+import common.SortingHelper;
+import sun.plugin.com.event.COMEventHandler;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class ShellSort {
+
+    private ShellSort(){
+
+    }
+
+    public static <E extends Comparable<E>> void sort2(E[] arr){
+
+        int h = arr.length/2;
+        while(h>=1){
+
+            for (int start = 0; start < h; start++) {
+
+                for (int i = start; i < arr.length; i+=h) {
+
+                    E temp = arr[i];
+                    int j;
+                    for ( j= i; j -h>=0 && temp.compareTo(arr[j-h])<0 ; j-=h) {
+
+                        arr[j] = arr[j-h];
+                    }
+                    arr[j] = temp;
+                }
+            }
+
+            h/=2;
+        }
+    }
+
+
+    public static <E extends Comparable<E>> void sort(E[] arr){
+
+        int n = arr.length;
+
+        // step表示间距(步长)
+        for (int step = n>>1; step >0 ; step>>=1) {
+
+            for (int start = 0; start < step; start++) {
+                insertionSort(arr,start,step);
+            }
+        }
+    }
+
+    private static <E extends Comparable<E>> void insertionSort(E[] arr,int start ,int step){
+
+        for (int i = start; i <arr.length; i+=step) {
+
+            E temp = arr[i];
+            int j;
+            for ( j = i; j > start && temp.compareTo(arr[j-step])<0 ; j-=step) {
+                arr[j]=arr[j-step];
+            }
+            arr[j] = temp;
+
+        }
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        int n = 1_00_000;
+        Integer[] test = ArrayGenerator.generatorRandomArray(n,n);
+        Integer[] test2 = Arrays.copyOf(test,test.length);
+        Integer[] test3 = Arrays.copyOf(test,test.length);
+        SortingHelper.sortTest(ShellSort.class,"sort",test);
+        SortingHelper.sortTest(ShellSort.class,"sort2",test2);
+        SortingHelper.sortTest(InsertionSort.class,"sort",test3);
+
+    }
+}
