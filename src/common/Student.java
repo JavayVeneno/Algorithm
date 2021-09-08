24c8d05740e7b7762646f038166936dbd88c6fbb
Veneno <747433822@qq.com>
2021-09-08 20:15:53 +0800

    更新代码

---
+++ src/common/Student.java
@@ -0,0 +1,28 @@
+package common;
+
+public class Student {
+    private int grade;
+    private String name;
+
+
+    public Student(int grade, String name) {
+        this.grade = grade;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
