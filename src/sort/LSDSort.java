d3c97d88c59b9e622d45657f17e365c909571854
Veneno <747433822@qq.com>
2021-10-25 21:43:50 +0800

    [第十四周][1025]停了快2个月了,这2个月一是在找资料(找了一周,最后花了20块找到了),二是在捣鼓新手机浪费了时间(9月15日开始关注然后玩到10月25日,需要好好反省反省)。本次更新简单实现了LSD排序,从最不重要的位置开始进行一个简单计数排序,直到最重要的一个元素排完,每次循环都依赖上一次计数排序本身的稳定性可以实现排序。

---
+++ src/sort/LSDSort.java
@@ -0,0 +1,76 @@
+package sort;
+
+import java.util.Comparator;
+import java.util.Random;
+
+public class LSDSort {
+
+    private LSDSort(){}
+
+    private static final int MAX = 256;
+
+    public static void sort(String[] words ,int W){
+
+        for (String word:words){
+            if(word.length()!=W){
+                throw new IllegalArgumentException("all elements's length must be same.");
+            }
+        }
+
+        for (int i = W-1; i >=0; i--) {
+
+            //从尾部开始每一位都执行一次计数排序
+            int[] count = new int[MAX];
+            int[] index = new int[MAX+1];
+
+            // 统计每个字母出现的次数
+            for (String word : words) {
+                count[word.charAt(i)]++;
+            }
+
+            // 计算每个字母的起始索引
+            for (int x = 0; x+1 < index.length ; x++) {
+                //下一个元素的起始索引= 当前元素的起始索引+当前元素的数量
+                index[x+1] = index[x]+count[x];
+            }
+
+            String[] copy = new String[words.length];
+
+            for (String word : words) {
+                // 根据该word的所排序的字母去找到他所对应的index
+                int pos = index[word.charAt(i)];
+                // 将其放入对应的区间
+                copy[pos] = word;
+                // 起始位置挪动一位
+                index[word.charAt(i)]++;
+            }
+
+            System.arraycopy(copy,0,words,0,words.length);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String[] students = new String[26*26*26];
+
+        for (int i = 0; i < students.length; i++) {
+            students[i]  = ""+ (char)(Math.random()*26+'a')+(char)(Math.random()*26+'a')+(char)(Math.random()*26+'a');
+        }
+
+
+
+        sort(students,3);
+        for (int i=0 ;i+1<students.length;i++){
+            if(students[i].compareTo(students[i+1])>0){
+                throw new RuntimeException("sort failed");
+            }
+//            if(students[i].getGrade()==students[i+1].getGrade()){
+//                if(students[i].getName().compareTo(students[i+1].getName())>0){
+//                    throw new RuntimeException("No stable counting sort");
+//                }
+//            }
+
+        }
+        System.out.println(students);
+    }
+}
