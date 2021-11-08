dd84e58532242131c590456ed3c9177b01398f58
Veneno <747433822@qq.com>
2021-11-08 14:55:15 +0800

    [第十五周][1108]细节上的优化,是很重要的。

--- src/match/SubstringMatch.java
+++ src/match/SubstringMatch.java
@@ -15,15 +15,21 @@ public class SubstringMatch {
             return 0;
         }
 
-        for (int i = 0; i < source.length(); i++) {
+        // 第一个优化点:当剩余源串长度小于子串时,可以终止匹配,并且可以处理target的length大于source的length的索引越界bug
+        for (int i = 0; i < source.length()-target.length()+1; i++) {
 
-            for (int j = 0; j < target.length(); j++) {
+            // 第二个优化点:内层循环中每次都得判断j是不是已经到了末尾,可以提出来在外层循环中判断,减少判断次数
+            int j = 0;
+            for ( ;j < target.length(); j++) {
                 if(source.charAt(i+j) != target.charAt(j)){
                     break;
                 }
-                if(j==target.length()-1){
-                    return i;
-                }
+//                if(j==target.length()-1){
+//                    return i;
+//                }
+            }
+            if(j==target.length()){
+                return i;
             }
         }
         return -1;
@@ -34,7 +40,7 @@ public class SubstringMatch {
         String source = "cloudStock";
         String target = "dS";
 
-        int match = bruteForce(source, target);
+        int match = bruteForce( source,target);
         System.out.println(source.substring(match,match+target.length()));
     }
 }
