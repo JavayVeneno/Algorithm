9663fcc200dbc933f3673b1691e48a7795067dd3
Veneno <747433822@qq.com>
2021-11-11 16:41:15 +0800

    [第十五周][1111]使用hash滚动的方式实现leetcode187号问题的解。

--- src/leetcode/match/Solution187.java
+++ src/leetcode/match/Solution187.java
@@ -21,8 +21,52 @@ class Solution187 {
         return new ArrayList<>(res);
     }
 
+
+
+    public List<String>  findRepeatedDnaSequences2(String dna ){
+
+        if(dna.length()<=10){
+            return new ArrayList<>();
+        }
+
+        // 将四个字段映射成1234
+        int[] map = new int[256];
+        String word = "ACGT";
+
+        for (int i = 0; i < word.length(); i++) {
+            map[word.charAt(i)] = i+1;
+        }
+
+
+
+        // 先算出9个字符的子串的hash
+        long hash = 0;
+        for (int i = 0; i < 9; i++) {
+            hash = hash *10  +map[dna.charAt(i)];
+        }
+
+
+        HashSet<Long> seen = new HashSet<>();
+        HashSet<String> res = new HashSet<>();
+
+        for (int i = 9; i < dna.length(); i++) {
+            hash = hash * 10 +  map[dna.charAt(i)];
+            if(seen.contains(hash)){
+                res.add(dna.substring(i-9,i+1));
+            }else{
+                seen.add(hash);
+            }
+            hash = hash - map[dna.charAt(i-9)] * 1000000000L;
+
+        }
+
+        return new ArrayList<>(res);
+
+
+    }
+
     public static void main(String[] args) {
-        String a = "AAAAAAAAAAA";
-        System.out.println(new Solution187().findRepeatedDnaSequences(a));
+        String a = "GAGAGAGAGAGA";
+        System.out.println(new Solution187().findRepeatedDnaSequences2(a));
     }
 }
