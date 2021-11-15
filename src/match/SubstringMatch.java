51071b2167c6d6ea70735edea36a8590948a1e89
Veneno <747433822@qq.com>
2021-11-15 17:03:35 +0800

    [第十五周][1115]使用滚动hash的思想完成字符串匹配(rabin-karp) 遗留的问题就是如果要找出剩余字符串还存在相同的target那么rk就会像暴力破解一样,出现性能问题(如 aaa...aaa,中找出所有含又aaa的子串,那么每次匹配最终都会循环完子串的长度)退化成O(m*n) ...

--- src/match/SubstringMatch.java
+++ src/match/SubstringMatch.java
@@ -12,6 +12,11 @@ public class SubstringMatch {
 
     private SubstringMatch(){}
 
+    private static final long B = 256;
+
+    private static final  long MOD = (long)1e9+7;
+
+
     public static int bruteForce(String source, String target)  {
 
         if(source == null || target == null){
@@ -42,25 +47,74 @@ public class SubstringMatch {
 
     }
 
+    public static int rabinKarp(String source,String target){
+
+        if(source == null || target == null){
+            throw new IllegalArgumentException("source and target must be not null");
+        }
+        if("".equals(target)){
+            return 0;
+        }
+
+        long powB = 1;
+        for (int i = 1; i < target.length(); i++) {
+            powB  = powB * B % MOD;
+        }
+
+
+        long tHash = 0;
+        for (int i = 0; i < target.length(); i++) {
+            tHash = (tHash * B + target.charAt(i))%MOD;
+        }
+
+        long currentHash = 0;
+        for (int i = 0; i < source.length() && i<target.length()-1; i++) {
+            currentHash = (currentHash* B + source.charAt(i)  )% MOD;
+        }
+
+        for (int i = target.length()-1; i <source.length() ; i++) {
+           currentHash = ( currentHash * B + source.charAt(i) )%MOD;
+           if(currentHash == tHash && equals(source,i-target.length()+1,i,target)){
+               return i-target.length()+1;
+           }
+           currentHash = ((currentHash - source.charAt(i-target.length()+1) * powB % MOD ) + MOD ) % MOD;
+        }
+        return -1;
+
+    }
+
+    private static boolean equals(String source, int left, int right, String target) {
+
+        for (int i = left; i <=right ; i++) {
+            if(source.charAt(i)!=target.charAt(i-left)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         String source = "cloudStock";
         String target = "dS";
 
         MatchHelper.matchTest(SubstringMatch.class,"bruteForce",source,target);
+        MatchHelper.matchTest(SubstringMatch.class,"rabinKarp",source,target);
 
         String words = FileOperation.readFile("src/PrideAndPrejudice.txt");
 
         MatchHelper.matchTest(SubstringMatch.class,"bruteForce",words,"china");
-        MatchHelper.matchTest(SubstringMatch.class,"bruteForce",words,"spring");
+        MatchHelper.matchTest(SubstringMatch.class,"rabinKarp",words,"china");
 
 
-        int n = 100000; int m = 1000;
+        int n = 1000000; int m = 5000;
 
         String aaa = IntStream.range(0, n).mapToObj(i -> "a").collect(Collectors.joining());
 
         String aab = IntStream.range(0, m - 1).mapToObj(i -> "a").collect(Collectors.joining("", "", "b"));
 
         MatchHelper.matchTest(SubstringMatch.class,"bruteForce",aaa,aab);
+        MatchHelper.matchTest(SubstringMatch.class,"rabinKarp",aaa,aab);
 
         //根据以上测试得出一个结论:在暴力匹配子串时,常规场景下,我们的匹配大多数都是提前终止的,且子串的长度都不会太长,所有性能是非常高的
         //但是在最后一个测试用例中可以看到在特殊的场景下,是会退化到 O(n * m)的
